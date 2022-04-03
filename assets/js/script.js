e164efde380e948d64a152a7843130317e6d1152
Caleeeb <daycinematography@gmail.com>
2022-04-02 22:22:41 -0400

    html structure complete, icon added, click to edit text

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,15 @@
 //text modification
+// when row is clicked the p is edited
+$(".row").on("click", "p", function () {
+    var text = $(this)
+        .text().trim();
+    // p is turned into a textarea
+    var textInput = $("<textarea>")
+        .addClass("w-100 h-50")
+        .val(text);
+    $(this).replaceWith(textInput);
+})
 
-// save button puts to lcoal story
+// save button puts to local storage
 
 // 
